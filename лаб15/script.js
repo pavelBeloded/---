816ab88d10f821620f49ab7e32516afdaa825a87
Pavel Beloded <beloded_2007@bk.ru>
2024-11-15 18:13:15 +0300

    end

--- лаб15/script.js
+++ лаб15/script.js
@@ -1,48 +1,84 @@
-let photos = document.querySelectorAll('.photo');
-photos[0]
-photos[0].addEventListener('mouseover', (e)=>{
-   photos[0].firstElementChild.style.display = 'none';
-   photos[0].lastElementChild.style.display = 'block';
+let timerId
+document.addEventListener('submit', (e)=>{
+	e.preventDefault()
+	let popup = document.querySelector('.pop_up')
+	let form = document.querySelector('.container.mail')
+	clearTimeout(timerId)
+	
+		// clearTimeout(timerId)
+		popup.classList.remove('closed')
+		popup.querySelector('p').innerHTML = "Данные были отправлены"
+		form.classList.add('closed')
+		
 
+		timerId = setTimeout(()=>{
+			popup.classList.add('closed')
+		}, 5000)
+
+	
+		document.querySelector('.main').classList.remove('hidden');
+		document.querySelector('.footer').classList.remove('hidden');
+
+		let inputs = document.forms[0];
+  		let fullName = "ФИ: " + inputs[0].value + " "+ inputs[1].value;
+
+		let birthday = "Дата рождения: " + inputs[2].value + '.' + inputs[3].value + '.' + inputs[4].value;
+		let faculty = "Факультет: " + document.forms[0][6][document.forms[0][6].value].innerHTML + " " + inputs[5].value + '-' + inputs[7].value;
+		let gender =  inputs[8].checked ? 'Пол: муж' : 'Пол: жен';
+		let mail = "Email: " + inputs[10].value + inputs[11].value;
+
+		let options = [fullName,birthday, gender, faculty, mail];
+
+
+		writeData(options);
 })
-photos[0].addEventListener('mouseout', (e)=>{
-    photos[0].firstElementChild.style.display = 'block';
-    photos[0].lastElementChild.style.display = 'none';
+
+
+
+function writeData(options){
  
+	options.forEach((item)=>{
+
+		let listItem = document.createElement('li');
+		listItem.className = "footerItem";
+		document.querySelector('.footer').firstElementChild.append(listItem);
+		listItem.innerHTML = item;
+	})
+
+}
+
+
+
+let photo1 = document.querySelectorAll(".gallery_photo")[0];
+let photo2 = document.querySelectorAll(".gallery_photo")[1];
+let photo3 = document.querySelectorAll(".gallery_photo")[2];
+
+photo1.parentElement.addEventListener('mouseover', (e)=>{
+	photo1.style.display = 'none';
+	document.querySelector('.description').style.display = 'block';
  })
 
- photos[1].addEventListener('mouseover', (e)=>{
-    photos[1].firstElementChild.style.border= '10px solid red';
+ photo1.parentElement.addEventListener('mouseout', (e)=>{
+	 photo1.style.display = 'block';
+	 document.querySelector('.description').style.display = 'none';
+  
+  })
+ 
+  photo2.addEventListener('click', (e)=>{
+	 photo2.classList.toggle('border');
+ 
+  
+  })
 
  
+  photo3.parentElement.addEventListener('mouseover', (e)=>{
+	photo3.classList.toggle('hidden')
+	photo3.nextElementSibling.classList.toggle('hidden')
  })
- photos[1].addEventListener('mouseout', (e)=>{
-    photos[1].firstElementChild.style.border= '0';
 
+ photo3.parentElement.addEventListener('mouseout', (e)=>{
+	photo3.classList.toggle('hidden')
+	photo3.nextElementSibling.classList.toggle('hidden')
   })
 
-
-  photos[2].addEventListener('mouseover', (e)=>{
-    photos[2].firstElementChild.setAttribute('src', './3.png');
- 
- })
- 
- photos[2].addEventListener('mouseout', (e)=>{
-    photos[2].firstElementChild.setAttribute('src', './2.png');
-
-  })
-
-
-  document.querySelector('#but').addEventListener('click', (e)=>{
-   e.preventDefault();
-   document.querySelectorAll('select').forEach((item, index)=>{
-      let value = item.value;
-
-      document.querySelectorAll('li')[index].innerHTML = value;
-   })
-  })
-
-
-
-
-
+  
